acccce2d15e2c351879f34b880e24f1d1198d23a
Matheus <josematheus1202@gmail.com>
2024-08-01 14:01:18 -0300

    tentativa

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ATVasp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
